f3fddf2e978c6413f70d7b2df7b3445c006129b0
Naif <50641595+nnnaif@users.noreply.github.com>
2020-08-21 14:49:14 +0300

    fix clearing downloads

--- chromium-broom/background.js
+++ chromium-broom/background.js
@@ -1,3 +1,8 @@
 'use strict';
-chrome.history.deleteAll(()=>{console.log("Cleared history.")});
-chrome.downloads.erase({}, ()=>{console.log("Cleared downloads.")})
+
+chrome.runtime.onStartup.addListener(() => {
+  // reloads extension because for some reason downloads.erase() doesnt work more than once.
+  chrome.runtime.reload();
+})
+chrome.history.deleteAll(() => { console.log("Cleared history.") });
+chrome.downloads.erase({}, () => { console.log("Cleared downloads.") })
